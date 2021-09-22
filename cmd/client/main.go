48915456c3b955713a8318b92e15d1f465bd833e
135yshr <isago@oreha.dev>
2021-09-22 20:05:32 +0900

    chg logic

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -4,33 +4,95 @@ import (
 	"context"
 	"fmt"
 	"github.com/135yshr/gRPC_Sample/pkg/helloworld"
+	"github.com/135yshr/gRPC_Sample/pkg/knockknock"
 	"google.golang.org/grpc"
+	"io"
 	"log"
 	"os"
 	"time"
 )
 
+var name = "World"
+
 func main() {
 	fmt.Println("Run client...")
+	if len(os.Args) > 1 {
+		name = os.Args[1]
+	}
 
 	conn, err := grpc.Dial("localhost:3000", grpc.WithInsecure(), grpc.WithBlock())
 	if err != nil {
 		log.Fatalf("Did not connect: %v", err)
 	}
 	defer conn.Close()
-	c:=helloworld.NewGreeterClient(conn)
 
+	gc := helloworld.NewGreeterClient(conn)
+	if err := sendGreeter(gc); err != nil {
+		log.Fatalf("Failed send greeter: %v", err)
+	}
+
+	waitc := make(chan struct{})
+	kc := knockknock.NewDoorClient(conn)
+	go func() {
+		err := sendKnock(kc)
+		if err != nil {
+			log.Fatalf("Failed send greeter: %v", err)
+		}
+		close(waitc)
+	}()
+
+	if err := sendGreeter(gc); err != nil {
+		log.Fatalf("Failed send greeter: %v", err)
+	}
+	time.Sleep(3 * time.Second)
+	if err := sendGreeter(gc); err != nil {
+		log.Fatalf("Failed send greeter: %v", err)
+	}
+	<-waitc
+}
+
+func receive(stream knockknock.Door_KnockClient) chan error {
+	cerr := make(chan error)
+	go func() {
+		for {
+			in, err := stream.Recv()
+			if err == io.EOF {
+				log.Println("EOF")
+				close(cerr)
+				return
+			}
+			if err != nil {
+				cerr <- err
+			}
+			fmt.Println(in.Message)
+		}
+	}()
+	return cerr
+}
+func sendKnock(kc knockknock.DoorClient) error {
+	stream, err := kc.Knock(context.Background())
+	if err != nil {
+		return err
+	}
+	req := &knockknock.KnockRequest{Count: 10, Wait: 1}
+	if err := stream.Send(req); err != nil {
+		return err
+	}
+	log.Println("Send knock")
+	select {
+	case e := <-receive(stream):
+		return e
+	}
+}
+
+func sendGreeter(gc helloworld.GreeterClient) error {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 
-	name := "World"
-	if len(os.Args) > 1 {
-		name = os.Args[1]
-	}
-
-	r, err := c.SayHello(ctx, &helloworld.HelloRequest{Name: name})
+	r, err := gc.SayHello(ctx, &helloworld.HelloRequest{Name: name})
 	if err != nil {
 		log.Fatalf("Could not greet: %v", err)
 	}
 	log.Println("Greeting:", r.GetMessage())
+	return nil
 }
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,9 +4,12 @@ import (
 	"context"
 	"fmt"
 	"github.com/135yshr/gRPC_Sample/pkg/helloworld"
+	"github.com/135yshr/gRPC_Sample/pkg/knockknock"
 	"google.golang.org/grpc"
+	"io"
 	"log"
 	"net"
+	"time"
 )
 
 func main() {
@@ -16,7 +19,9 @@ func main() {
 		log.Fatalf("Failed to listen %v", err)
 	}
 	s := grpc.NewServer()
-	helloworld.RegisterGreeterServer(s, newServer())
+	ss := &server{}
+	helloworld.RegisterGreeterServer(s, ss)
+	knockknock.RegisterDoorServer(s, ss)
 	if err := s.Serve(l); err != nil {
 		log.Fatalf("Failed to serve: %v", err)
 	}
@@ -24,15 +29,54 @@ func main() {
 
 type server struct {
 	helloworld.UnimplementedGreeterServer
+	knockknock.UnimplementedDoorServer
 }
 
-func (h server) SayHello(_ context.Context, req *helloworld.HelloRequest) (*helloworld.HelloReply, error) {
+func (s *server) SayHello(_ context.Context, req *helloworld.HelloRequest) (*helloworld.HelloReply, error) {
 	return &helloworld.HelloReply{
 		Message: fmt.Sprintf("Hello %s", req.GetName()),
 	}, nil
 }
 
-func newServer() *server {
-	s := &server{}
-	return s
+func (s *server) Knock(stream knockknock.Door_KnockServer) error {
+	for {
+		log.Println("Wait recv")
+		in, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+		log.Println("Receive request")
+		count := in.Count
+		if count == 0 {
+			count = 100
+		}
+		wait := time.Duration(in.Wait) * time.Second
+		if wait == 0 {
+			wait = time.Second
+		}
+
+		select {
+		case err := <-send(stream, int(count), wait):
+			return err
+		default:
+			time.Sleep(time.Second)
+		}
+	}
+}
+
+func send(stream knockknock.Door_KnockServer, count int, wait time.Duration) chan error {
+	cerr := make(chan error)
+	go func() {
+		for i := 0; i < count; i++ {
+			if err := stream.Send(&knockknock.KnockReply{Message: "knock!"}); err != nil {
+				cerr <- err
+			}
+			time.Sleep(wait)
+		}
+		close(cerr)
+	}()
+	return cerr
 }
